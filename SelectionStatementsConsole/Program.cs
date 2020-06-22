e44dfc9949ead4a5f9ee42a2d739a38b2892624e
Brenton Phillips <phillips.brenton@gmail.com>
2020-06-22 14:24:07 -0500

    SelectionStatement Excercise

---
+++ SelectionStatementsConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SelectionStatementsConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the number guessing game!!!");
+            Console.WriteLine("See if you can guess the correct number.");
+            Console.WriteLine("What is the upper limit for the range of the number?");
+
+
+            var userResponse = Console.ReadLine();
+
+            var upperLimit = int.Parse(userResponse);
+
+
+            var random = new Random();
+            var favNumber = random.Next(1, upperLimit);
+
+            Console.WriteLine("Input your guess:");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > favNumber)
+            {
+                Console.WriteLine("Your guess is too high! Sorry, try again.");
+            }
+            else if (guess < favNumber)
+            {
+                Console.WriteLine("Your guess is too low! Sorry, try again.");
+            }
+            else
+            {
+                Console.WriteLine("Correct!!!");
+            }
+
+
+
+        }
+    }
+}
